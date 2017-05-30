07cc89159fb92f00e16ddd981a088db07584c6a8
xufeng <xufengyongyou@163.com>
2017-05-30 15:11:56 +0800

    模拟

--- cookie.py
+++ cookie.py
@@ -1,12 +1,24 @@
-
+import urllib
 import urllib2
 import cookielib
 
-cookie=cookielib.MozillaCookieJar()
-cookie.load('cookie.txt',ignore_discard=True,ignore_expires=True)
-req=urllib2.Request("http://www.baidu.com")
-handler=urllib2.HTTPCookieProcessor(cookie)
-opener=urllib2.build_opener(handler)
-response=opener.open(req)
-print response.read()
-#cookie.save(ignore_discard=True,ignore_expires=True)
+filename='cookie.txt'
+
+cookie=cookielib.MozillaCookieJar(filename)
+opener=urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
+postdata=urllib.urlencode({
+    'stdid':'5454',
+    'pwd':'35445'
+})
+
+loginurl='http://www.baidu.com'
+#模拟登陆
+result=opener.open(loginurl,postdata)
+#保存cookie到本地
+cookie.save(ignore_discard=True,ignore_expires=True)
+
+#目标url
+finalurl='http://www.baidu.com'
+
+result=opener.open(finalurl)
+print result.read()
