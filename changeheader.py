7fd1502fd93f2c87ddbfaad53e1b057b5d196cf9
xufeng <xufengyongyou@163.com>
2017-05-30 14:34:13 +0800

    错误打印

--- changeheader.py
+++ changeheader.py
@@ -7,8 +7,14 @@ user_ageent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 
 values={'username':'cqc','password':'xxx'}
 headers={'User-Agent':user_ageent}
 data=urllib.urlencode(values)
-
+#add urlerror
 request=urllib2.Request(url,data,headers)
-response=urllib2.urlopen(request)
+try:
+    response=urllib2.urlopen(request)
+except urllib2.urlparse,e:
+    print e.code
+    print e.reason
+else:
+    print "ok"
 page=response.read()
 print page
